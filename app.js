7422c223244a5b087045c6d78c6709428a57e8d4
YF <yfeldman@live.com>
2021-11-05 14:54:04 +0000

    not working version

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
-//const { initializeApp, cert } = require('firebase-admin/app');
-const admin = require('firebase-admin');
+const { initializeApp, cert } = require('firebase-admin/app');
+//const admin = require('firebase-admin');
 
 const app = express()
 
